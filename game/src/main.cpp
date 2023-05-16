4f4d19573709ae9cffbd5ce5061bb3301ec0f1cb
Tomf1 <Tomf1@DESKTOP-29NOB14>
2023-05-15 22:57:41 -0400

    got pictures loading and drawing on mouse

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,10 +2,14 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+    
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
+
+    Texture2D  texture = LoadTexture("../game/assets/textures/letterKenny.png");//loading image using the path
+   Sound sound = LoadSound("../game/assets/audio/laser.mp3");
 
     while (!WindowShouldClose())
     {
@@ -14,15 +18,24 @@ int main(void)
         DrawText("Hello World!", 16, 9, 20, RED);
         EndDrawing();
        
+        texture.height = SCREEN_HEIGHT;
+        texture.width = SCREEN_WIDTH;
+        DrawTexture(texture, 0, 0,RAYWHITE);//drawing the image
+
+        if (IsKeyPressed(32))
+        {
+        PlaySound(sound);
+
+        }
+
         DrawPixel(10, 10, BLACK);
         
 
         DrawCircle(100, 100, 50, BLUE);
         DrawRectangle(500, 500, 50, 50, RED);
         DrawLine(200, 200, 400, 200, YELLOW);
+        DrawRectangle(GetMouseX(), GetMouseY(), 100, 100,WHITE);//this will draw a rectangle at the position of the mouse useing GetMouseX and GetMouseY functions 
 
-       Texture2D  texture = LoadTexture("game/assets/textures/plane_diffuse.png");
-         DrawTexture(texture, 90, 90, BLACK);
 
        if( IsKeyPressed(49))  //this checks is if key 49 or "1" on the ascii table is presed if so it changes is key presseed to true allow the user to use keyboard inputs 
         {
