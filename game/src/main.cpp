b6c355a919dca9ef118be309d5853e9337df2f47
TFaron1 <tomf1911@hotmail.com>
2023-05-25 11:48:43 -0400

    working

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,6 +7,26 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
 
+    InitAudioDevice();//to get sounds to work you must include initaudiodevice
+    
+    float vol = 1.0f;
+                             ///LOAD///
+   
+    Texture2D  texture = LoadTexture("../game/assets/textures/letterKenny.png");//loading image
+
+    Sound sound = LoadSound("../game/assets/audio/chaChing.mp3");// load sound effect
+    Music bgmusic = LoadMusicStream("../game/assets/audio/sweet.mp3");//load music          
+
+    Font font = LoadFont("../game/assets/fonts/bitCheese.ttf");//laods a new font  
+
+    Color color = BLUE;
+    Color recCol = BLUE;
+
+    Rectangle rec1;
+    Rectangle  rec2;
+   
+        PlayMusicStream(bgmusic);//plays music 
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
