8274e69d2e664d05c3bc022a3c2c1af11935c214
TFaron1 <tomf1911@hotmail.com>
2023-06-08 12:27:59 -0400

    Lab 4 lecture done

---
+++ game/src/TileMap.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "TileCoord.h"
+#include "raylib.h"
+
+
+const unsigned int  MAP_WIDTH = 24;
+const unsigned int MAP_HEIGHT = 12;
+
+enum Tile
+{
+	Floor = 0,
+	Wall,
+	Count
+	
+};
+
+class TileMap
+{
+public:
+	int tileSizeX = 32;
+	int tileSizeY = 32;
+
+	Tile tiles[MAP_WIDTH][MAP_HEIGHT];
+	Color tileColors[Tile::Count];
+
+	TileMap()
+	{
+		tileColors[Floor] = GRAY;
+		tileColors[Wall] = ORANGE;
+	}
+
+	Vector2 ScreenPosOfTile(TileCoord tilePosition)
+	{
+		return { (float)tilePosition.x * tileSizeX, (float)tilePosition.y * tileSizeY };
+	}
+
+	unsigned int GetWidth()
+	{
+		return MAP_WIDTH;
+	}
+
+
+	unsigned int GetHeight()
+	{
+		return MAP_HEIGHT;
+	}
+
+	
+
+	void DrawTiles()
+	{
+		for (int x = 0; x < GetWidth(); x++)
+		{
+			for (int y = 0; y < GetHeight(); y++)
+			{
+				Vector2 position = ScreenPosOfTile({ x,y });
+				Tile tile = tiles[x][y];
+				Color color = WHITE;
+				if (tile == Floor) color = GREEN;
+				else if(tile == Wall) color = ORANGE;
+				DrawRectangleV(position, { (float)tileSizeX, (float)tileSizeY }, color);
+			}
+		}
+	}
+};
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,17 +1,59 @@
 #include "rlImGui.h"
+#include "TileMap.h"
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+float generateWallChance = 0.2;
+
+TileMap map;
+
+void RandomTiles(TileMap& level, float chanceOfWall = 0.2)
+{
+	for (int x = 0; x < level.GetWidth(); x++)
+	{
+		for (int y = 0; y < level.GetHeight(); y++)
+		{
+
+
+			if (GetRandomValue(0.0, 1.0)  < chanceOfWall)
+			{
+				level.tiles[x][y] = Wall;
+			}
+			else 
+			{
+				level.tiles[x][y] = Floor;
+			}
+		}
+	}
+}
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
+	rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
+
+		rlImGuiBegin();
+
+		ImGui::SliderFloat("wall chance", &(generateWallChance), 0, 0.2);
+
+		if(ImGui::Button("Randomize map"))
+		{
+			RandomTiles(map, generateWallChance);
+
+		}
+
+
+		rlImGuiEnd();
+		
+		map.DrawTiles();
+
         EndDrawing();
     }
 
