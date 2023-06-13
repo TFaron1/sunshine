060f025d7d55eb001116a93a45ae88bd9a088776
TFaron1 <tomf1911@hotmail.com>
2023-06-12 22:05:25 -0400

    added seeking and fleeing

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,7 +1,9 @@
 #include "rlImGui.h"
 #include "Math.h"
+#include <vector>
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+
 
 Vector2 WrapAroundScreen(Vector2 position)
 {
@@ -25,13 +27,13 @@ int main(void)
     Vector2 acceleration = { 0, 50 };//px/s/s
     float maxSpeed = 1000;
     float maxAccel = 1000;
-
+   
     while (!WindowShouldClose())
     {
         const float dt = GetFrameTime();
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 16, 9, 20, RED);
+        DrawText("Mode 1: food", 16, 9, 20, RED);
        
         rlImGuiBegin();
         ImGui::SliderFloat2("position", &(position.x), 0, SCREEN_WIDTH);
@@ -39,11 +41,25 @@ int main(void)
         ImGui::DragFloat2("acceleration", &(acceleration.x), 1, -maxAccel, maxAccel);
         rlImGuiEnd();
 
+        
+        Vector2 place = GetMousePosition();
+
+        if (IsMouseButtonPressed(0))
+        {
+            DrawCircleV(place, 10, BLUE);
+        }
+        
         Vector2 displacement = velocity * dt;
         position = position + displacement + acceleration * 0.5 * dt * dt;
         velocity = velocity + acceleration * dt;
         velocity = velocity + acceleration * dt;
-       
+        acceleration = Normalize(place - position) * 500 - velocity;
+
+
+        if (IsMouseButtonDown(1))
+        {
+            acceleration = Negate(Normalize(position - place) * 500 - velocity);
+        }
         DrawCircleV(position, 50, RED);
 
        position = WrapAroundScreen(position);
