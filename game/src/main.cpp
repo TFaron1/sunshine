c6dd8f14256a2297e46135fa85be53e4dc45c06f
TFaron1 <tomf1911@hotmail.com>
2023-06-12 20:47:05 -0400

    started A1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -6,12 +6,18 @@ int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
     SetTargetFPS(60);
+    rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
+
+        rlImGuiBegin();
+        
+        rlImGuiEnd();
+
         EndDrawing();
     }
 
