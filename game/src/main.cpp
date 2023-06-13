24216038887e015b592b00a1423ace6ad4167205
TFaron1 <tomf1911@hotmail.com>
2023-06-12 20:53:56 -0400

    start

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -13,7 +13,7 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
-
+       
         rlImGuiBegin();
         
         rlImGuiEnd();
